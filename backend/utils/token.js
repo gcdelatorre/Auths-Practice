7cf2e83ec26fa92bce64e2adc0d6a6fac71794bd
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-01-08 22:48:32 +0800

    create send access and refresh token fun

--- backend/utils/token.js
+++ backend/utils/token.js
@@ -9,3 +9,17 @@ export const createAccessToken = (userId) => {
 export const createRefreshToken = (userId) => {
     return jsonwebtoken.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
 };
+
+export const sendAccessToken = (req, res, accessToken) => {
+    res.status(200).json({
+        accessToken,
+        email: req.body.email
+    });
+}
+
+export const sendRefreshToken = (res, refreshToken) => {
+    res.cookie("refreshtoken", refreshToken, {
+        httpOnly: true,
+        path: "/refresh_token",
+    });
+}
