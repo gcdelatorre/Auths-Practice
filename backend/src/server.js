b98fe99c6a9db93d16e88e09b0c88096b0c54fd3
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-01-08 19:19:26 +0800

    register controller

--- backend/src/server.js
+++ backend/src/server.js
@@ -39,3 +39,24 @@ app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
 })
 
+// Register account controller
+app.post("/register", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "User already exists" });
+
+        const hashedPassword = await hash(password, 10)
+
+        const newUser = await User.create({
+            email,
+            password: hashedPassword,
+        })
+
+        res.status(200).json({ message: "User created successfully", data: newUser, success: true });
+
+    } catch (err) {
+        res.status(500).json({ message: err.message, success: false });
+    }
+})
