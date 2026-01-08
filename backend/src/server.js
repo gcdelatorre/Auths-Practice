50aff9cecf37a07cc7d1f1295ff4d5056fa88a9b
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-01-08 23:08:44 +0800

    create login controller applying tokens

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+// library imports
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import jsonwebtoken from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+
+// config imports
 import connectDB from "../config/db.js";
 
+// db imports
 import User from "../models/user.model.js";
+
+// utils imports
+import { createAccessToken, createRefreshToken, sendAccessToken, sendRefreshToken } from "../utils/token.js";
 
 connectDB();
 
@@ -35,28 +42,64 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
-})
-
-// Register account controller
+// 1 Register account controller
 app.post("/register", async (req, res) => {
-    try {
-        const { email, password } = req.body;
+    const { email, password } = req.body;
 
+    try {
+        // find user
         const user = await User.findOne({ email });
         if (user) return res.status(400).json({ message: "User already exists" });
 
+        // crpyt password
         const hashedPassword = await hash(password, 10)
 
+        // create user
         const newUser = await User.create({
             email,
             password: hashedPassword,
         })
 
+        // send response
         res.status(200).json({ message: "User created successfully", data: newUser, success: true });
 
     } catch (err) {
         res.status(500).json({ message: err.message, success: false });
     }
+})
+
+// 2 Login account controller
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // find user
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "User does not exist" });
+
+        // check if valid password
+        const isPasswordValid = await compare(password, user.password)
+        if (!isPasswordValid) return res.status(400).json({ message: "Invalid credentials" });
+
+        // create access and refresh token
+        const accessToken = createAccessToken(user._id);
+        const refreshToken = createRefreshToken(user._id);
+
+        // update refresh token in database
+        user.refreshToken = refreshToken;
+        await user.save();
+
+        sendRefreshToken(res, refreshToken);
+        sendAccessToken(req, res, accessToken);
+
+    } catch (err) {
+        res.status(500).json({ message: err.message, success: false });
+    }
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 })
