db98fcf55fb10f70500124aa5121eebf451e4b61
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-01-15 03:48:07 +0800

    ui skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Protected from './pages/Protected';
+import Logout from './pages/Logout';
 
-
-export default function App() {
-
+function App() {
   return (
     <>
-      <div className="bg-slate-200 h-screen flex items-center justify-center">
-        App
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/protected" element={<Protected />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
     </>
-  )
+  );
 }
 
+export default App;
