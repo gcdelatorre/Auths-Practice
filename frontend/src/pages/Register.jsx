46c348a10c4e57c68b6bb06df99fe31f717246d5
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-01-16 02:04:12 +0800

    create register function

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { register } from '../api/auth.api.js';
 
 function Register() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const passwordMatch = password === confirmPassword;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (passwordMatch) {
+      try {
+        await register(email, password)
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+      } catch (err) {
+        alert(err.response.data.message); // just for testing, toast in production
+      }
+    }
+    else {
+      alert('Passwords do not match');
+    }
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    const { name, value } = e.target
+
+    if (name === 'email') setEmail(value);
+    if (name === 'password') setPassword(value);
+    if (name === 'confirmPassword') setConfirmPassword(value);
+
+  }
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleSubmit}>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
@@ -13,6 +48,9 @@ function Register() {
             id="email"
             type="email"
             placeholder="Email"
+            value={email}
+            onChange={handleChange}
+            name="email"
           />
         </div>
         <div className="mb-6">
@@ -24,12 +62,29 @@ function Register() {
             id="password"
             type="password"
             placeholder="******************"
+            value={password}
+            onChange={handleChange}
+            name="password"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+            Confirm Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="confirmPassword"
+            type="password"
+            placeholder="******************"
+            value={confirmPassword}
+            onChange={handleChange}
+            name="confirmPassword"
           />
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
+            type="submit"
           >
             Sign Up
           </button>
