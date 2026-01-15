63af1284ab0e2617eb35d80c9c0def43b284cf79
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2026-01-16 02:10:08 +0800

    create register

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -79,12 +79,13 @@ function Register() {
             value={confirmPassword}
             onChange={handleChange}
             name="confirmPassword"
-          />
+            />
         </div>
         <div className="flex items-center justify-between">
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
+            disabled={!email || !password || !confirmPassword || !passwordMatch}
           >
             Sign Up
           </button>
